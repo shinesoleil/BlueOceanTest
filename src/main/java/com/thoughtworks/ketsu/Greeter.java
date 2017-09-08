11f8e6941447c7b38634502d52c3680198f1ba6c
hpan <hao_pan@outlook.com>
2017-09-08 10:23:47 +0800

    added one bac practice

--- src/main/java/com/thoughtworks/ketsu/Greeter.java
+++ src/main/java/com/thoughtworks/ketsu/Greeter.java
@@ -4,4 +4,8 @@ public class Greeter {
     public String greet(String name) {
         return "Hello " + name;
     }
+
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
